1bb096b309bde759849e794c9d3e7e9675f6710f
keith <zeo4500@gmail.com>
2022-05-23 22:07:52 -0700

    Start to populate command line arguments.

--- src/executables/cliExecutor/main.cpp
+++ src/executables/cliExecutor/main.cpp
@@ -2,8 +2,34 @@
 #include "DriverFactory.hpp"
 #include "DriverInterface.hpp"
 
+#include "boost/program_options.hpp"
+
+#include <iostream>
+#include <vector>
+
+namespace po = boost::program_options;
+
 int main( int argc, char** argv )
 {
+
+    po::options_description desc( "Options" );
+    desc.add_options()
+        ( "help", "produce help message" )
+        ( "driver", po::value< std::string >(), "Driver to execute" )
+    ;
+
+    po::variables_map vm;
+    po::parsed_options parsed = po::command_line_parser( argc, argv ).options(desc).allow_unregistered().run();
+    po::store( parsed, vm );
+    po::notify( vm );
+
+    std::vector< std::string > pass_to_driver = po::collect_unrecognized( parsed.options, po::include_positional );
+
+    if ( vm.count( "help" ) )
+    {
+        std::cout << desc << std::endl;
+        return 0;
+    }
 
     std::unique_ptr< kac::cudalearn::DriverInterface > driver = kac::cudalearn::DriverFactory::GetFactory().ConstructMachine( "SimpleDriver" );
 
